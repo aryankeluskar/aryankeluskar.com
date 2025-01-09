90969e942bafe54f4c99859ef50c9cf0d05e41ea
Aryan Keluskar <aryankeluskar6@gmail.com>
2025-01-09 15:12:39 -0700

    added todos

---
+++ readme.md
@@ -0,0 +1,25 @@
+# aryankeluskar.github.io
+
+This is the repository for my personal website. It is a static website that is hosted on GitHub Pages. The website is a simple, multi-page website that serves as my portfolio.
+
+- Scripting: [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+- Styling: [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+- Deployment: [GitHub Pages](https://pages.github.com/), (Future: [Vercel])
+- Domain: [Cloudflare Registrar](https://www.cloudflare.com/products/registrar/)
+- SSL: Vercel
+- Analytics: [dub.sh](https://dub.sh)
+
+# TODO
+
+- [x] Add a favicon
+- [ ] Make the hero image semi-elliptical
+- [ ] Have hyperlinks and images to hometowns
+- [x] Update the resume link
+- [ ] Add projects to the projects page
+- [ ] Paralax scrolling on the home page
+- [ ] Use `Solway` font
+- [ ] Blueprint-like grid pattern in the background
+- [ ] Add Buttons to Resume, Projects, and Contact below hero
+- [ ] Expand on hackathon wins and conferences
+- [ ] Add color
+- [ ] Cycling adventures(?)
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,15 +2,16 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
-  initials: "DV",
-  url: "https://dillion.io",
-  location: "San Francisco, CA",
+  name: "Aryan Keluskar",
+  initials: "AK",
+  url: "https://aryankeluskar.com",
+  location: "Tempe, AZ",
   locationLink: "https://www.google.com/maps/place/sanfrancisco",
   description:
-    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+    "18, i build scalable fast backends and ios apps. i love to meet people; develop impactful fast software; and to innovate & inspire!",
   summary:
-    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+      "I'm an 18 year old college junior with a passion for problem solving, learning intricate concepts, and building products. I was first exposed to coding when I was 10. Solving problems by converting my thoughts to tangible results in languages that feel incredibly cryptic has been one of the most amazing feelings ever. ",
+    // "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
   avatarUrl: "/me.png",
   skills: [
     "React",
